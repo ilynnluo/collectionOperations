d7dc957f1e8298cc2a60521eef0ab1bcd2af70b7
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2024-04-23 22:57:14 -0400

    Add project files.

---
+++ collectionOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace collectionOperations
+{
+    public class QCollection<T>
+    {
+        private Queue<T> items;
+        public QCollection()
+        {
+            items = new Queue<T>();
+        }
+        public void Enqueue(T item)
+        {
+            items.Enqueue(item);
+        }
+        public T Dequeue()
+        {
+            if(items.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            return items.Dequeue();
+        }
+        public List<T> Descending()
+        {
+            return items.OrderByDescending(item => item).ToList();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QCollection<int> q = new QCollection<int>();
+            q.Enqueue(1);
+            q.Enqueue(10);
+            q.Enqueue(5);
+            q.Enqueue(9);
+
+            List<int> descendQ = q.Descending();
+            foreach(int item in descendQ)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
